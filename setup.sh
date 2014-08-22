f60bffd4466b787e4de339cc5e7c2bbcba5c7206
Adam Walker <adamwalker10@gmail.com>
2014-08-22 17:11:48 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git submodule init
+git submodule update
+
+cabal sandbox init
+cabal sandbox add-source lib/bdd lib/bv lib/code-widget lib/debug lib/graph-draw lib/haskell_cudd lib/hast lib/synthesis lib/tsl lib/util tools/termite-app
