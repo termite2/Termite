904ee71204bf8796738b9874012dc8624d3e4aa2
Adam Walker <adamwalker10@gmail.com>
2014-09-26 23:16:32 -0400

    add cabal update to setup script

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ git submodule init
 git submodule update
 
 cabal sandbox init
+cabal update
 cabal sandbox add-source lib/bdd lib/bv lib/code-widget lib/debug lib/graph-draw lib/haskell_cudd lib/hast lib/synthesis lib/tsl lib/util tools/termite-app
