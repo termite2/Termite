ce9e35ff62c6fea6fc2efa431db505848023679a
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2014-10-01 15:15:39 -0400

    don't copy cudd makefiles; use -f instead

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 pushd lib/cudd
 
 if [ "`getconf LONG_BIT`" == "64" ]; 
-    then cp Makefile.64bit Makefile;
-    else cp Makefile.32bit Makefile;
+    then make -f Makefile.64bit
+    else make -f Makefile.32bit
 fi
 
-make libso
 popd
 
 LD_LIBRARY_PATH=`pwd`/lib/cudd/libso cabal install termite --extra-include-dirs=`pwd`/lib/cudd/include --extra-lib-dirs=`pwd`/lib/cudd/libso --bindir=`pwd`/bin 
