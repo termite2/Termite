199c17e9a7673bb3689eed8321612ba07fd37c69
Adam Walker <adamwalker10@gmail.com>
2014-10-08 13:13:46 -0400

    install build dependencies in setup script

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ git submodule update
 cabal sandbox init
 cabal update
 cabal sandbox add-source lib/bdd lib/bv lib/code-widget lib/debug lib/graph-draw lib/haskell_cudd lib/hast lib/synthesis lib/tsl lib/util tools/termite-app
+
+cabal install c2hs
+cabal install gtk2hs-buildtools
+
