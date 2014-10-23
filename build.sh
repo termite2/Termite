778b33574e116325292817f51d64d8b7d86d710e
David Cock <David.Cock@nicta.com.au>
2014-10-23 17:44:45 +1100

    Standalone termite binary
    Building CUDD as a native Haskell package lets us avoid dynamic lib
    dependencies.

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-pushd lib/cudd
-
-if [ "`getconf LONG_BIT`" == "64" ]; 
-    then make -f Makefile.64bit libso
-    else make -f Makefile.32bit libso
-fi
-
-popd
-
-LD_LIBRARY_PATH=`pwd`/lib/cudd/libso cabal install termite --extra-include-dirs=`pwd`/lib/cudd/include --extra-lib-dirs=`pwd`/lib/cudd/libso --bindir=`pwd`/bin 
-
+cabal install termite --bindir=`pwd`/bin 
